95af5350898f708a2c1de496684d601402cb99e3
rjrocha19 <rodolfo_rj@live.com>
2023-02-12 23:21:48 -0300

    Atulizando repo

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold} from '@expo-google-fonts/inter';
+
+import { Loading } from './src/Components/Loading';
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  const [fontsLoaded] = useFonts ({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded){
+    <Loading />
+  }
+
+  return (
+    <>
+      <Home /> 
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
+  );
+}
