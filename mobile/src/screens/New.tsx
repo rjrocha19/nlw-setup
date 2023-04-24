f67730aa8bf9dd2c1af3bab6292b6677ae214496
rjrocha19 <rodolfo_rj@live.com>
2023-04-23 21:34:32 -0300

    atulização de repo

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -24,7 +24,7 @@ export function New() {
   async function handleCreateNewHabit() {
     try {
       if(!title.trim() || weekDays.length ===0){
-        Alert.alert('Novo Hábito', 'Informe o nome do hábito e escolha a frequencia.')
+        return Alert.alert('Novo Hábito', 'Informe o nome do hábito e escolha a frequencia.')
       }
 
       await api.post('/habits', { title, weekDays });
